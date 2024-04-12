08333280580e2a83303de55fe68495727cd2c7dc
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2024-04-12 14:59:55 +0530

    changed some styles

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,15 +3,15 @@ import { arrow, hero_image, hero_image2 } from "../assets";
 
 const Hero = () => {
   return (
-    <div className="hero relative flex flex-row bg-gradient-to-b from-[#ddd6f3] to-[#faaca8] py-20 px-8">
+    <div className="hero flex flex-row h-auto md:h-auto lg:h-[120vh] bg-gradient-to-b from-[#ddd6f3] to-[#faaca8] py-20 px-5">
       <div className="hero-left w-2/4 lg:flex-1 flex flex-col justify-center items-start lg:items-center gap-[30px] leading-[1] z-10">
         <div className="w-full lg:w-2/4 new_arrivals flex justify-start lg:justify-start text-start items-start lg:items-start">
           <h2 className="uppercase text-[#090909] text-[20px] text-nowrap lg:text-wrap lg:text-[20px] font-[600]">
             New Arrivals only
           </h2>
         </div>
-        <div className="w-full lg:w-2/4 flex flex-col justify-center items-start text-wrap lg:text-nowrap text-[#171717] text-[35px] lg:text-[75px] font-[700] top-20">
-          <p className="leading-[2.5rem] lg:leading-[5rem]">
+        <div className="w-full lg:w-2/4 flex flex-col justify-center items-start text-wrap lg:text-nowrap text-[#171717] text-[35px] md:text-[65px] lg:text-[75px] font-[700] top-20">
+          <p className="leading-[2.5rem] md:leading-[4rem] lg:leading-[5rem]">
             New <br className="break hidden lg:flex" /> Collections{" "}
             <br className="break hidden lg:flex" /> For Everyone
           </p>
@@ -21,7 +21,7 @@ const Hero = () => {
           <img src={arrow} alt="arrow_icon" />
         </div>
       </div>
-      <div className="hero-right w-2/4 flex justify-end lg:justify-start items-end lg:items-center pl-0 lg:pl-16">
+      <div className="hero-right relative w-2/4 flex justify-end lg:justify-start items-end lg:items-center pl-8 lg:pl-16 left-10 top-10 md:left-[0px]">
         <img
           className="max-w-[250px] lg:max-w-[100%] lg:w-[78%] mt-[0px] lg:mt-[120px]"
           src={hero_image2}
