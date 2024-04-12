1f14690a0cc9d77e78b03472a728051da8a7aed4
abhishekGhosh99 <125865872+abhishekGhosh99@users.noreply.github.com>
2024-04-13 00:45:55 +0530

    Added styles for responsiveness

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoginSignup = () => {
   return (
     <div className="loginsignup w-full h-auto bg-[#F2B8BD] py-[100px]">
-      <div className="loginsignup-container w-[580px] h-auto bg-white m-auto py-[40px] px-[60px] rounded-3xl">
+      <div className="loginsignup-container w-full lg:w-[580px] h-auto bg-white m-auto py-[40px] px-[40px] lg:px-[60px] rounded-3xl">
         <h1 className="flex justify-center text-[30px] font-[500]">Sign Up</h1>
         <div className="loginsignup-fields flex flex-col gap-[29px] mt-[30px]">
           <input
@@ -22,7 +22,7 @@ const LoginSignup = () => {
             placeholder="Password"
           />
         </div>
-        <button className="w-[460px] h-[60px] text-white bg-[#ff4141] my-8 rounded-2xl text-[18px] font-[500] cursor-pointer">
+        <button className="w-full lg:w-[460px] h-[60px] text-white bg-[#ff4141] my-8 rounded-2xl text-[18px] font-[500] cursor-pointer">
           Continue
         </button>
         <p className="loginsignup-login mt-[20px] text-[#5c5c5c] text-[15px] font-[500]">
